d8bec2a8aec493ddf70128a8208b1b0ca1ee0ec7
Rachael Momber <rlmomber@gmail.com>
2024-06-03 16:50:06 -0400

    refactor: change all models to reference external db

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ApplicationRecord
-  #self.table_name = 'rec_lis_recipes'
-  belongs_to :user#, foreign_key: 'rec_lis_user_id'
+  self.table_name = 'rec_lis_recipes'
+  belongs_to :user, foreign_key: 'rec_lis_user_id'
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
-  has_many :bookmarks#, foreign_key: 'rec_lis_recipe_id'
-  validates :user, presence: true#, foreign_key: 'rec_lis_user_id'
+  has_many :bookmarks, foreign_key: 'rec_lis_recipe_id'
+  validates :user, presence: true, foreign_key: 'rec_lis_user_id'
 end
