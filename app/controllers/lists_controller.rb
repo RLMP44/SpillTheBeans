0df0f0b6b8a162af4c6c86a17034855425f03d1a
Rachael Momber <rlmomber@gmail.com>
2024-06-07 15:03:12 -0400

    add: create and destroy added to lists_controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,19 +1,36 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: :show
+  before_action :set_list, only: %i[show destroy]
 
   def index
-    # @lists = List.all
-    # Preloads title to help wiht N+1 queries
-    @lists = List.includes(:title)
+    @lists = List.all
+    @bookmark = Bookmark.new
   end
 
   def show
     @bookmarks = @list.bookmarks.all
   end
 
+  def create
+    list = List.new(list_params)
+    list.user = current_user
+    if list.save
+      redirect_to list_path(list)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+  end
+
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:title, :comment)
   end
 end
