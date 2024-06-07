190cf44196fd339fd059c0fc6be0c2f2c9e6392b
Rachael Momber <rlmomber@gmail.com>
2024-06-07 13:49:11 -0400

    debugging

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,14 @@ puts 'User created'
 
 puts 'Creating recipes and lists...'
 cake = Recipe.create!(user: rachael, name: 'Chocolate cake', description: 'A rich chocolate cake', rating: 8.5)
+puts 'cake'
 desserts = List.create!(title: 'Desserts', user: rachael, comment: 'Really good list')
+puts 'desserts'
 Bookmark.create!(recipe: cake, list: desserts, comment: 'Yummmm!')
+puts 'first bookmark'
 pesto = Recipe.create!(user: rachael, name: 'Pesto Bowl', description: 'Chicken, tomato, and avocado over quinoa and spinach rice', rating: 9)
+puts 'pesto'
 dinners = List.create!(title: 'Dinners', user: rachael, comment: 'my go-to for dinners')
+puts 'dinners'
 Bookmark.create!(recipe: pesto, list: dinners, comment: "Aaron's favorite")
 puts 'Done.'
