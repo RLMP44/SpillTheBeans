87437882638a343665322f9c6c151de05859e365
Rachael Momber <rlmomber@gmail.com>
2024-06-08 23:33:26 -0400

    update: add set_list and @list to bookmarks_controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,14 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: :create
+
   def create
     bookmark = Bookmark.new(bookmark_params)
+    bookmark.list = @list
     if bookmark.save
-      redirect_to list_path
+      redirect_to list_path(@list)
     else
-      render :show, status: :unprocessable_entity
+      render 'lists/show', status: :unprocessable_entity
     end
   end
 
@@ -19,5 +23,9 @@ class BookmarksController < ApplicationController
 
   def set_bookmark
     @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
