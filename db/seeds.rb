2e834ccf2a2953ec7cedd9f9d69459e34d5b9376
Rachael Momber <rlmomber@gmail.com>
2024-06-11 10:23:20 -0400

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,54 @@ rachael = User.create!(email: 'rach@me.com', password: '123456')
 puts 'User created'
 
 puts 'Creating recipes and lists...'
-cake = Recipe.create!(user: rachael, name: 'Chocolate cake', description: 'A rich chocolate cake', rating: 8.5)
+cake = Recipe.create!(
+  user: rachael,
+  name: 'Chocolate cake',
+  description: 'A rich chocolate cake',
+  rating: 8.5,
+  cook_time: 45,
+  total_time: 60,
+  difficulty: 5,
+  oven_temp: 180,
+  oven: true,
+  ingredients:
+    "cocoa powder, vanilla oil, olive oil, 2 eggs, flour, cinnamon, boiling water,
+    salt",
+  instructions:
+    "Mix all dry ingredients. Add eggs, olive oil, vanilla, and mix.
+    Stir in boiling water and mix to make mixture airy. Cook in rice cooker."
+)
 puts 'cake'
 desserts = List.create!(title: 'Desserts', user: rachael, comment: 'Really good list')
 puts 'desserts'
 Bookmark.create!(recipe: cake, list: desserts, comment: 'Yummmm!')
 puts 'first bookmark'
-pesto = Recipe.create!(user: rachael, name: 'Pesto Bowl', description: 'Chicken, tomato, and avocado over quinoa and spinach rice', rating: 9)
+pesto = Recipe.create!(
+  user: rachael,
+  name: 'Pesto Bowl',
+  description: 'Chicken, tomato, and avocado over quinoa and spinach rice',
+  rating: 9,
+  cook_time: 45,
+  total_time: 60,
+  difficulty: 3,
+  oven_temp: 220,
+  oven: true,
+  ingredients:
+    '⅓ cup prepared pesto, 2 tablespoons balsamic vinegar,
+    1 tablespoon extra-virgin olive oil, ½ teaspoon salt, ¼ teaspoon ground pepper,
+    2 chicken breasts, 4 cups arugula, 2 cups cooked quinoa,
+    1 cup halved cherry tomatoes, 1 diced avocado',
+  instructions:
+    "STEP 1 Cook quinoa/rice
+    STEP 2 Whisk pesto, vinegar, oil, salt and pepper in a large bowl.
+    Remove 4 tablespoons of the mixture to a small bowl; set both bowls aside.
+    STEP 3 Cook whole chicken breast wrapped in foil in oven with salt and pepper.
+    STEP 4 Add arugula and quinoa/rice to the large bowl with the vinaigrette
+    and toss to coat. Divide the arugula mixture between 4 bowls.
+    Top with tomatoes, avocado and chicken.
+    Drizzle each bowl with 1 tablespoon of the reserved pesto mixture.
+    Feel free to add additional vegetables and swap the chicken for shrimp, steak, tofu, etc."
+)
 puts 'pesto'
 dinners = List.create!(title: 'Dinners', user: rachael, comment: 'my go-to for dinners')
 puts 'dinners'
