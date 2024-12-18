1a7624196060642d6ed1a88d59a5eceda2c01d98
Rachael Momber <rlmomber@gmail.com>
2024-12-18 11:01:32 +0900

    add: top recipes and lists to main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @lists = List.all
+    @top_recipes = Recipe.get_top_recipes
+    @recipes = Recipe.all
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,6 +6,10 @@ class Recipe < ApplicationRecord
   has_many :ingredients
   has_many :bookmarks#, foreign_key: 'rec_lis_recipe_id'
   # validates :user, presence: true, foreign_key: 'rec_lis_user_id'
+  scope :top_recipes, lambda { |recipe|
+    recipe.order()
+    where()
+  }
 
   # def user_id
   #   rec_lis_user_id
@@ -15,9 +19,23 @@ class Recipe < ApplicationRecord
     username
   end
 
+  def self.get_top_recipes
+    self.tally_bookmarks
+  end
+
   private
 
   def username
     user.username
+  end
+
+  def self.tally_bookmarks
+    hash = {}
+    Recipe.all.each do |recipe|
+      hash[recipe] = recipe.bookmarks.count
+    end
+    sorted_hash = hash.sort_by { |key, value| value }.to_h
+    top_ten = sorted_hash.keys.last(10)
+    top_ten
   end
 end
