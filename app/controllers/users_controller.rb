540c548b5e8e96caefb4c301b302baba72bdb203
Rachael Momber <rlmomber@gmail.com>
2025-01-04 00:23:56 +0900

    add: setup profile page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: :show
+  before_action :set_user, only: :show
 
   def show
+    @recipes = @user.recipes
+    @lists = @user.lists
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
