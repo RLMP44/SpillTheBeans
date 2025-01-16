deee25a436caaeb23cd0f6bc6985c710b16037f3
Rachael Momber <rlmomber@gmail.com>
2025-01-15 22:09:56 -0500

    add: prep capabilities to add new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,14 +3,29 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.all
+    @recipe = Recipe.new
   end
 
   def show
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      redirect_to recipe_path(@recipe), notice: 'recipe saved.'
+    else
+      render :index, status: :unprocessable_entity, alert: 'Save failed'
+    end
   end
 
   private
 
   def set_recipe
     @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :cook_time, :difficulty, :oven_temp, :oven, :total_time, :ingredients, :instructions)
   end
 end
