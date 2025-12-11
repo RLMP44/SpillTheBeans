6040695ab2524f094e5bc5bdb76aae88373ee857
Rachael Momber <rlmomber@gmail.com>
2025-12-11 14:23:35 -0500

    add: react integration and Checklist component

---
+++ app/javascript/components/Checklist.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function Checklist({ ingredients }) {
+  console.log(ingredients)
+  return (
+    <div className="form-check">
+      {ingredients.map((ingredient) => {
+        return (
+          <div key={ingredient.id} id={`check-${ingredient.id}`} >
+            <input className="form-check-input" type="checkbox" value="" />
+            <label className="form-check-label" htmlFor={`check-${ingredient.id}`}>
+              {ingredient.amount} {ingredient.name}
+            </label>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
---
+++ app/javascript/controllers/react_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+import React from "react"
+import { createRoot } from "react-dom/client"
+import Checklist from "../components/Checklist.jsx"
+
+export default class extends Controller {
+  connect() {
+    const root = createRoot(this.element)
+    const ingredientsAsJSON = this.element.dataset.reactIngredients
+    const ingredients = JSON.parse(ingredientsAsJSON)
+    console.log('hi')
+    console.log(root)
+    root.render(<Checklist ingredients={ingredients} />)
+  }
+}
